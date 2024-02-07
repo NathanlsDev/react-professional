7f912a3c466271f4341b3a5d3ac1be9f2cee8f88
Nathan Lima <nathanls.dev@gmail.com>
2024-02-07 20:40:21 -0300

    returning components

---
+++ 2-calculator/calculator.js
@@ -0,0 +1,31 @@
+const Header = () => <div class="hearder">Calculator</div>;
+const Calculator = () => {
+  return (
+    <div class="calculator">
+      <div class="display"></div>
+      <div class="keyboard">
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>0</button>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Calculator />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
