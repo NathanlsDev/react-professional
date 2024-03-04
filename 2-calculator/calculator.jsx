cd439585be4672bd624b30ac1208f4ff28d565ea
Nathan Lima <nathanls.dev@gmail.com>
2024-03-04 15:23:22 -0300

    release: final project

--- 2-calculator/calculator.jsx
+++ 2-calculator/calculator.jsx
@@ -1,34 +1,80 @@
 const calcRegex = /^([0-9]|-|\+|\*|\/|\.)*$/;
 
+const keyPad = [
+  7, 8, 9, "/", "C",
+  4, 5, 6, "*", "Del",
+  1, 2, 3, "-", "Ad",
+  0, ".", "+", "=",
+];
+
+const advancedKeyPad = ["(", ")", "Hist", "Up", "AC"];
+
 const Header = () => <div className="hearder title">Calculator</div>;
 
 const Button = ({ value, onClick, className }) => (
-  <button
-    onClick={() => {
-      onClick(value);
-    }}
-    className={className}
-  >
+  <button onClick={() => {
+    onClick(value);
+  }} className={className}>
     {value}
   </button>
 );
 
 const Calculator = () => {
-  const keyPad = [
-    7,  8,  9,  "/",  "C",
-    4,  5,  6,  "*",  "Del",
-    1,  2,  3,  "-",  "",
-    0,  ".",  "+",  "=",
-  ];
+  const [display, setDisplay] = React.useState("");
+  const [error, setError] = React.useState();
+  const [showAdvanced, toggleAdvanced] = React.useState(false);
+  const [history, setHistory] = React.useState([]);
+  const [showHistory, toggleHistory] = React.useState(false);
+  const histRef = React.useRef();
 
-  const doTheMath = () => setDisplay(eval(display));
+  React.useEffect(() => {
+    if (error) {
+      setError();
+    }
+  }, [display]);
+
+  React.useEffect(() => {
+    if (history.length > 2 && showHistory) {
+      histRef.current.scrollTop = histRef.current.scrollHeight;
+    }
+  }, [history.length, showHistory]);
+
   const clearDisplay = () => setDisplay("");
 
-  const handleClick = (value) => {
+  const doTheMath = () => {
+    try {
+      const result = eval(display);
+      setHistory(
+        [].concat(history, {
+          calculation: display,
+          result,
+        })
+      );
+      setDisplay(`${result}`);
+    } catch (err) {
+      setError(`Invalid Expression, ${err.message}`);
+    }
+  };
+
+  const lastExpression = () => {
+    const lastIndex = history.length - 1;
+    if (lastIndex > -1) {
+      const newDisplay = history[lastIndex].calculation;
+      const newHistory = history.slice(0, lastIndex);
+      setDisplay(newDisplay);
+      setHistory(newHistory);
+    }
+  };
+
+  const handleClick = value => {
     const actions = {
       "=": () => doTheMath(),
       C: () => clearDisplay(),
       Del: () => setDisplay(display.slice(0, -1)),
+      Ad: () => toggleAdvanced(!showAdvanced),
+      Hist: () => toggleHistory(!showHistory),
+      AC: () => clearDisplay(setHistory([])),
+      Up: () => lastExpression(),
       default: () => setDisplay(`${display}${value}`),
     };
 
@@ -36,10 +82,8 @@ const Calculator = () => {
     action();
   };
 
-  const [display, setDisplay] = React.useState("");
-
-  const generateButtonNumbers = () =>
-    keyPad.map((keyPad, index) => {
+  const generateButtons = arr =>
+    arr.map(keyPad => {
       const span2Class = keyPad === 0 ? "span2" : "";
       const primaryClass = isNaN(keyPad) ? "primary" : "";
 
@@ -47,7 +91,7 @@ const Calculator = () => {
         <Button
           className={`${span2Class}${primaryClass}`}
           value={keyPad}
-          key={index}
+          key={keyPad}
           onClick={handleClick}
         />
       );
@@ -55,17 +99,24 @@ const Calculator = () => {
 
   return (
     <div className="calculator">
+      {showHistory && (
+        <div ref={histRef} className="history">
+          {history.map(({ calculation, result }, index) => (
+            <p key={index}>{`${calculation} = ${result}`}</p>
+          ))}
+        </div>
+      )}
       <input
         type="text"
         className="display"
         value={display}
-        onChange={(event) => {
+        onChange={event => {
           const { value } = event.target;
           if (calcRegex.test(value)) {
             setDisplay(event.target.value);
           }
         }}
-        onKeyDown={(event) => {
+        onKeyDown={event => {
           if (event.key === "Enter") {
             doTheMath();
           }
@@ -74,7 +125,11 @@ const Calculator = () => {
           }
         }}
       />
-      <div className="keyboard">{generateButtonNumbers()}</div>
+      {error && <p className="error">{error}</p>}
+      {showAdvanced && (
+        <div className="keyboard">{generateButtons(advancedKeyPad)}</div>
+      )}
+      <div className="keyboard">{generateButtons(keyPad)}</div>
     </div>
   );
 };
